2f6e660af69f1c843a7d9de9ff94c7d6664a5fdf
ninhnguyen375 <ninhnguyen375@gmail.com>
2019-01-26 11:52:12 +0700

    nothing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Hello World, Hello World
+              Hello World
             </Text>
           </View>
 
